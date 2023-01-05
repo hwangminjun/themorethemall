aa865bf29766869c0e4a8af8ac8d2659c3fce157
hwangminjun <hmj7102@naver.com>
2023-01-05 16:23:17 +0900

    방금 만든 따끈따끈

---
+++ src/main/java/com/tmtm/TmtmApplication.java
@@ -0,0 +1,13 @@
+package com.tmtm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmtmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmtmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tmtm/TmtmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmtm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmtmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
