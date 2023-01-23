cfcb51674175a1bdb974612386bf37567c70d121
kim_hyun_seob <khs97425@naver.com>
2023-01-23 16:46:16 +0900

    시설 오류 완료

--- src/main/java/com/tmtm/fac/FacManegeController.java
+++ src/main/java/com/tmtm/fac/FacManegeController.java
@@ -3,22 +3,14 @@ package com.tmtm.fac;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import javax.servlet.http.HttpSession;
-import javax.websocket.Session;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
@@ -106,6 +98,7 @@ public class FacManegeController {
 		map.put("page", page);
 		return map;
 	}
+	
 	
 	
 	
